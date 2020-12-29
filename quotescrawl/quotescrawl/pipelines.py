6e4b51d5682d6d1aa67150b933d668060e767213
sbhusal123 <suryabhusal11@gmail.com>
2020-12-29 09:57:15 +0545

    Basic working with pipelines.

--- quotescrawl/quotescrawl/pipelines.py
+++ quotescrawl/quotescrawl/pipelines.py
@@ -7,7 +7,18 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 
+"""
+Enabling Pipeline
+    - Add pipeline to the setting's ITEM_PIPELINES with priority as a number
+
+How does it works:
+    - After crawling the item, the items are returned to the pipline as per the priority
+    - Then process the items i.e storing to the database
+"""
+
 
 class QuotescrawlPipeline:
     def process_item(self, item, spider):
+        print("Pipeline: " + item['title'][0])
+        print(item)
         return item
